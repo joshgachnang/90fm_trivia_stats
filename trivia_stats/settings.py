b49dc8f184dfbbd2beaf202a35db2247d60d4580
Josh Gachnang <josh@pcsforeducation.com>
2013-04-07 14:34:03 -0500

    Updating bootstrap to use /static/

--- trivia_stats/settings.py
+++ trivia_stats/settings.py
@@ -71,7 +71,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     # os.path.join(PROJECT_DIR, 'static'),
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'trivia_stats/static'),
 )
 
 # List of finder classes that know how to find static files in
