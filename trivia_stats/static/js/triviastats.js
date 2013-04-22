fafd79f3ae2fa19291bff242a0144a7ae3aee6ed
Josh Gachnang <josh@pcsforeducation.com>
2013-04-22 13:44:50 -0500

    Updating for 2014 countdown.

--- trivia_stats/static/js/triviastats.js
+++ trivia_stats/static/js/triviastats.js
@@ -1,5 +1,5 @@
 function homepage_init() {
-    var now, trivia_start, trivia_start_count_up;
+    var now, trivia_start, trivia_start_count_up, trivia_2014_start, trivia_end;
 //    Placeholder fix for IE9.
     $('input, textarea').placeholder();
     // Set up countdown/countup widget
@@ -7,13 +7,20 @@ function homepage_init() {
 //    var now = new Date(2013, 3, 19, 19, 0, 0, 0);
     // Month is 3 because Javascript has Jan = 0..
     trivia_start = new Date(2013, 3, 19, 18, 0, 0, 0);
+    trivia_start = new Date(2014, 3, 11, 18, 0, 0, 0);
     // Since hour 1 starts right away..
     trivia_start_count_up = new Date(2013, 3, 19, 17, 0, 0, 0);
+    trivia_end = new Date(2013, 3, 22, 0, 0, 0, 0);
 //    console.log("Now: ", now, " Trivia Start: ", trivia_start);
     if (now < trivia_start) {
 //        console.log("Not trivia time yet!");
 //        $("#current_hour").html("<h3>Trivia Countdown</h3>");
         $("#countdown").countdown({until: trivia_start, format: 'dHMS', layout: '<div class="cur_days"><p>{dn}</p><p>{dl}</p></div>' +
+            '<div class="cur_hours chms">{hn}</div>' +
+            '<div class="cur_minutes chms">{mn}</div>' +
+            '<div class="cur_seconds chms">{sn}</div>'});
+    }else if (now > trivia_end) {
+        $("#countdown").countdown({until: trivia_2014_start, format: 'dHMS', layout: '<div class="cur_days"><p>{dn}</p><p>{dl}</p></div>' +
             '<div class="cur_hours chms">{hn}</div>' +
             '<div class="cur_minutes chms">{mn}</div>' +
             '<div class="cur_seconds chms">{sn}</div>'});
